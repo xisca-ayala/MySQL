df92f42d17dd610966e1021033f7c84c9874d575
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-02-01 11:19:26 +0100

    reto3

--- mysql.js
+++ mysql.js
@@ -60,6 +60,74 @@ async function main(){
         // console.log("datos modificados");
         // console.log(result);
 
+//RETO1 5.2:
+
+        // let sql = "SELECT AVG (mark) FROM markss WHERE subjects_id = 1 "; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos recogidos");
+        // console.log(result);
+
+        // let sql = "SELECT COUNT(*) FROM students"; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos recogidos");
+        // console.log(result);
+
+        // let sql = "SELECT * FROM centroEstudios.groups "; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos recogidos");
+        // console.log(result);
+        
+        // let sql = "DELETE FROM markss WHERE mark > 5 AND (date > '2022-12-31' OR date <'2024-01-01')"; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos borrados");
+        // console.log(result);
+
+        // let sql = " SELECT * FROM students WHERE start_date > '2023-12-31' "; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos recogidos");
+        // console.log(result);
+
+        // let sql = " SELECT subject_id, COUNT(*) AS num_teachers FROM subject_teacher  GROUP BY subject_id"; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos recogidos");
+        // console.log(result);
+
+
+
+//RETO2 5.2:
+
+        // let sql = "SELECT students_id, mark FROM markss WHERE students_id BETWEEN 1 AND 20OR (mark > 8 AND date BETWEEN '2022-12-31' AND '2024-01-01');  "; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos recogidos");
+        // console.log(result);
+
+        // let sql = "SELECT subjects_id, AVG (mark) AS media FROM markss WHERE date > '2023-01-31'GROUP BY subjects_id "; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos recogidos");
+        // console.log(result);
+
+        // let sql = "SELECT students_id, AVG (mark) AS media FROM markss WHERE date > '2023-01-31'GROUP BY students_id "; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos recogidos");
+        // console.log(result);
+
+//RETO 3:
+        // let sql = "SELECT students.first_name, students.last_name, subjects.title FROM students " +
+        // " INNER JOIN markss ON students.student_id = markss.students_id " +
+        // " INNER JOIN subjects ON markss.subjects_id = subjects.subject_id "; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos recogidos");
+        // console.log(result);
+
+        // let sql = "SELECT teachers.first_name, teachers.second_name, subjects.title " +
+        // " FROM teachers INNER JOIN subject_teacher  " +
+        // " ON teachers.teacher_id = subject_teacher.teacher_id "+
+        // "INNER JOIN subjects ON subject_teacher.subject_id = subjects.subject_id"; 
+        // let [result] = await connection.query(sql);
+        // console.log("datos recogidos");
+        // console.log(result);
+
+
 
     }
     catch(err){
