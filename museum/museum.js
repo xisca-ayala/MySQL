41007984708c6d4eeca25141c306f447acea9388
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-02-09 12:30:46 +0100

    corrreccion museo

--- museum/museum.js
+++ museum/museum.js
@@ -10,18 +10,24 @@ async function main(){
         });
         console.log("Conexion correcta")
 
+
+        // let params = ["en prestamo"];
         // let sql = "SELECT pieces.name, location.location, property_status.loan_date, owner.name,owner.last_name, owner.email, owner.direction" +
-        // "FROM pieces INNER JOIN location ON pieces.id_location = location.id_location" +
-        // "INNER JOIN property_status ON pieces.id_property = property_status.id_property" +
-        // "INNER JOIN owner ON pieces.id_owner = owner.id_owner"; 
+        // "FROM pieces"  +
+        // "INNER JOIN location ON pieces.id_location = location.id_location " +
+        // "INNER JOIN property_status ON pieces.id_property = property_status.id_property " +
+        // "INNER JOIN owner ON pieces.id_owner = owner.id_owner " +
+        // "WHERE property_status.status = ? ";
         // let [result] = await connection.query(sql);
         // console.log("datos recogidos");
         // console.log(result);
+       
 
-        // let sql = " SELECT type_colection, COUNT(*) AS pieces_number FROM pieces" +
-        // "INNER JOIN colections ON pieces.id_colection= colections.id_colection" +
-        // "GROUP BY colections.type_colection" +
-        // "ORDER BY pieces_number DESC"; 
+
+        // let sql = " SELECT type_colection, COUNT(*) AS pieces_number FROM pieces " +
+        // "INNER JOIN colections ON pieces.id_colection= colections.id_colection " +
+        // "GROUP BY colections.type_colection " +
+        // "ORDER BY pieces_number DESC "; 
         // let [result] = await connection.query(sql);
         // console.log("datos recogidos");
         // console.log(result);
